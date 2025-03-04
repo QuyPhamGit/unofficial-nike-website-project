472e4caf2b16151331a02d9949436b8927a314d7
Quy Pham Ngo Thien <quy81947@gmail.com>
2025-03-04 15:32:00 -0500

    Add files via upload

---
+++ unofficial-nike-website/data/products2.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    pro_name: "Nike-Air-Trainer-1-SP",
+    pro_desc: "Made to take you places, the original cross trainer from '87 now lets you cross between uptown and downtown without a glitch.",
+    pro_price: 3519000,
+    pro_img: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/bca2ba20-3d0c-481b-b7a0-6cda4178b5ce/air-trainer-1-sp-shoes-6QJXK2.png",
+    pro_cat: "Nike Shoe",
+    pro_url: "http://localhost/demo-mindx/main-page/Nike-Air-More-Up-tempo-96.php",
+  },
+  {
+    id: 2,
+    pro_name: "Nike-Air-More-Up-tempo-96",
+    pro_desc: "With '90s graffiti-styled graphics alongside visible Air cushioning, the Air More Uptempo '96 is all about off-court looks.",
+    pro_price: 4999000,
+    pro_img: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/2e20b4bc-9a65-415d-88d6-dafbd743fff8/air-more-uptempo-96-shoes-tWJ7rk.png",
+    pro_cat: "Nike Shoe",
+    pro_url: "http://localhost/demo-mindx/main-page/Nike-Air-Trainer-1-SP.php",
+  },
+  {
+    id: 3,
+    pro_name: "Nike Air Max 90 SE",
+    pro_desc: "Max Air changed the game in '87. Now, we honour its emerald anniversary (35 years!) with the Nike Air Max 90 SE.",
+    pro_price: 3829000,
+    pro_img: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/bddcdbff-904e-496e-9f61-ad1f82bd961c/air-max-90-se-shoes-j59HGz.png",
+    pro_cat: "Nike Shoe",
+    pro_url: "http://localhost/demo-mindx/main-page/Nike-Air-Max-90-SE.php",
+  }
+]
+
+
